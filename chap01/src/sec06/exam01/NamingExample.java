37dbac8899b33150e89508bcfda60cac2b268372
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-03-24 10:33:50 +0900

    :)

---
+++ chap01/src/sec06/exam01/NamingExample.java
@@ -0,0 +1,10 @@
+package sec06.exam01;
+
+public class NamingExample {
+/*
+ 
+ 	class 이름은 무조건 파스칼 케이스기법★★★★★( 첫문자가 대문자인 기법 ) >> BusTaxiBike
+ 		-> 프로그램은 돌아가지만 기존의 명제를 어기게 되므로 클래스명은 반드시 대문자로 시작할 것!
+ 	
+ 	나머지(변수 등)는 카멜 케이스 기법 사용( 첫문자도 소문자인 기법 ) >> busTaxiBike*/
+}
